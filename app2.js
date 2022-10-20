45c51c73c2fb9b6042b624be9353b38080b6e531
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-10-19 22:35:46 -0700

    10/19 update
    HTML completed on index & index2; styling on styles &style2 completed. JS in app2 is also completed.

---
+++ app2.js
@@ -0,0 +1,14 @@
+//Staff Page JavaScript
+const panels = document.querySelectorAll('.panel');
+//console.log(panels);
+panels.forEach((panels)=>{
+    panels.addEventListener('click', () =>{
+        removeActiveClass()
+        panels.classList.add('active')
+    })
+})
+function removeActiveClass(){
+    panels.forEach(panels=>{
+        panels.classList.remove('active')
+    })
+}
